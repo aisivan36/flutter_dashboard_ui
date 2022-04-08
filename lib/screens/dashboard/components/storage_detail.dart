4eee0cfa8bcffefd5d481c4fc313d1cda7d6ed1c
ivan-boginski <aisivan36@gmail.com>
2022-04-08 08:29:51 +0700

    storage detail

---
+++ lib/screens/dashboard/components/storage_detail.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:web_dashboard/constants.dart';
+import 'package:web_dashboard/screens/dashboard/components/chart.dart';
+import 'package:web_dashboard/screens/dashboard/components/storage_info.dart';
+
+class Storagedetails extends StatelessWidget {
+  const Storagedetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      decoration: const BoxDecoration(
+        color: secondaryColor,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Storage Datails',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: defaultPadding),
+          Chart(),
+          StorageInfoCard(
+            svgSrc: "assets/icons/Documents.svg",
+            title: "Documents Files",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/media.svg",
+            title: "Media Files",
+            amountOfFiles: "15.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/folder.svg",
+            title: "Other Files",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/unknown.svg",
+            title: "Unknown",
+            amountOfFiles: "1.3GB",
+            numOfFiles: 140,
+          ),
+        ],
+      ),
+    );
+  }
+}
